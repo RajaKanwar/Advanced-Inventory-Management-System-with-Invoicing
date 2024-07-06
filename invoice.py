0dac8181a4c70cfa90e9b727a8f4f410c6756b6e
RajaKanwar <87748117+RajaKanwar@users.noreply.github.com>
2024-07-06 14:54:03 +0530

    Update invoice.py

--- invoice.py
+++ invoice.py
@@ -9,9 +9,11 @@ class Invoice:
         pdf = FPDF()
         pdf.add_page()
         pdf.set_font("Arial", size=12)
+        # Represent PDF file "Which important point will be in PDF file"
         pdf.cell(200, 10, txt=f"Invoice for Sale ID: {self.sale.transaction_id}", ln=True, align='C')
         pdf.cell(200, 10, txt=f"Product ID: {self.sale.product_id}", ln=True)
         pdf.cell(200, 10, txt=f"Quantity Sold: {self.sale.quantity}", ln=True)
         pdf.cell(200, 10, txt=f"Sale Price: {self.sale.sale_price}", ln=True)
         pdf.cell(200, 10, txt=f"Date: {self.sale.date}", ln=True)
-        pdf.output(f"Invoice_{self.sale.transaction_id}.pdf")
+        
+        pdf.output(f"Invoice_{self.sale.transaction_id}.pdf") # this line is represent "genrating PDF"
