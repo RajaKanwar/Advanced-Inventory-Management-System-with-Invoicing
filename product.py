7503df60a2dbc5615f78eee9eae52dd38452b259
RajaKanwar <87748117+RajaKanwar@users.noreply.github.com>
2024-07-06 15:17:01 +0530

    Update product.py

--- product.py
+++ product.py
@@ -1,7 +1,8 @@
 class Product:
     def __init__(self, product_id, name, category, price, quantity):
-        self.product_id = product_id
-        self.name = name
-        self.category = category
-        self.price = price
-        self.quantity = quantity
+        # Initialize the Product with given attributes
+        self.product_id = product_id  # Unique identifier for the product
+        self.name = name  # Name of the product
+        self.category = category # Category to which the product belongs
+        self.price = price # Price of the product
+        self.quantity = quantity  # Quantity of the product in stock
