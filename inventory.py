4c69167fa16bdec8483755d9b6de5f297d391cd5
RajaKanwar <87748117+RajaKanwar@users.noreply.github.com>
2024-07-06 15:03:23 +0530

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -1,9 +1,9 @@
 class Inventory:
     def __init__(self):
-        self.products = {}
+        self.products = {} # this line calls Product.py 
 
     def add_product(self, product):
-        self.products[product.product_id] = product
+        self.products[product.product_id] = product # This line will call product.py and the product to be added will be added according to the point of product.py.
 
     def update_product(self, product_id, name=None, category=None, price=None, quantity=None):
         if product_id in self.products:
@@ -16,9 +16,9 @@ class Inventory:
             if quantity is not None:
                 self.products[product_id].quantity = quantity
 
-    def remove_product(self, product_id):
+    def remove_product(self, product_id): # First check the product ID. If it matches the product ID in the product list, then remove it.
         if product_id in self.products:
             del self.products[product_id]
 
-    def view_products(self):
+    def view_products(self): # call product present values or product 
         return self.products.values()
