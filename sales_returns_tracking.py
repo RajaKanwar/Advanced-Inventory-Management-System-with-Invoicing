b895f9a54eeb3a94e544992ace8297c5846e6505
RajaKanwar <87748117+RajaKanwar@users.noreply.github.com>
2024-07-06 15:20:14 +0530

    Update sales_returns_tracking.py

--- sales_returns_tracking.py
+++ sales_returns_tracking.py
@@ -1,15 +1,15 @@
 from transaction import Sale, Return
 class SalesReturnsTracking:
     def __init__(self):
-        self.sales = []
-        self.returns = []
+        self.sales = [] # List to store sales records
+        self.returns = [] # List to store return records
         
 # sale and create transaction id for sale sale record 
     def record_sale(self, transaction_id, product_id, quantity, sale_price, date):
-        sale = Sale(transaction_id, product_id, quantity, sale_price, date)
-        self.sales.append(sale)
+        sale = Sale(transaction_id, product_id, quantity, sale_price, date) # This line for Create a Sale object
+        self.sales.append(sale)  # Append the sale record to the sales list
         
 # return product using transation id and a reson for" why return this product "
     def record_return(self, transaction_id, product_id, quantity, reason, date):
-        return_record = Return(transaction_id, product_id, quantity, reason, date)
-        self.returns.append(return_record)
+        return_record = Return(transaction_id, product_id, quantity, reason, date)# This line for Create a Return object
+        self.returns.append(return_record) # Append the return record to the returns list
