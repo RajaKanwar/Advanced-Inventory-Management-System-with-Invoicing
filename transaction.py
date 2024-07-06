a8159e4297ea0c361cf3b59229a1cd7b45d1ee77
RajaKanwar <87748117+RajaKanwar@users.noreply.github.com>
2024-07-06 15:18:41 +0530

    Update transaction.py

--- transaction.py
+++ transaction.py
@@ -6,11 +6,11 @@ class Transaction:
         self.date = date
 
 class Sale(Transaction):
-    def __init__(self, transaction_id, product_id, quantity, sale_price, date):
-        super().__init__(transaction_id, product_id, quantity, date)
-        self.sale_price = sale_price
+    def __init__(self, transaction_id, product_id, quantity, sale_price, date): 
+        super().__init__(transaction_id, product_id, quantity, date) # the sale transaction by calling the parent Transaction class's initializer
+        self.sale_price = sale_price # Set the sale price for this transaction
 
 class Return(Transaction):
     def __init__(self, transaction_id, product_id, quantity, reason, date):
-        super().__init__(transaction_id, product_id, quantity, date)
-        self.reason = reason
+        super().__init__(transaction_id, product_id, quantity, date)   #  the return transaction by calling the parent Transaction class's initializer
+        self.reason = reason # Set the reason for the return
